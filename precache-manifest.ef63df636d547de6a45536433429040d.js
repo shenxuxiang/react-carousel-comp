e4ffad3c8e0747aeabe0b8e7249045261d8533ba
shenxuxiang <594521765@qq.com>
2019-03-06 22:38:49 +0800

    Updates

---
+++ precache-manifest.ef63df636d547de6a45536433429040d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d9c2c0a0ac5e554a410",
+    "url": "/react-carousel-comp/static/js/runtime~main.7d9c2c0a.js"
+  },
+  {
+    "revision": "99b8f78172851689ff1c",
+    "url": "/react-carousel-comp/static/js/main.99b8f781.chunk.js"
+  },
+  {
+    "revision": "63a903a44c3352c3ed69",
+    "url": "/react-carousel-comp/static/js/1.63a903a4.chunk.js"
+  },
+  {
+    "revision": "d0b836ddcbc1452e3d224b850eac1001",
+    "url": "/react-carousel-comp/static/images/15.d0b836dd.jpg"
+  },
+  {
+    "revision": "aea87554bd401ebd614e5ce31170f40c",
+    "url": "/react-carousel-comp/static/images/14.aea87554.jpg"
+  },
+  {
+    "revision": "e76cdd13dd261a08d7b4206f46aacbaf",
+    "url": "/react-carousel-comp/static/images/13.e76cdd13.jpg"
+  },
+  {
+    "revision": "e1882d280a5c82619bf4df83973c85bc",
+    "url": "/react-carousel-comp/static/images/12.e1882d28.jpg"
+  },
+  {
+    "revision": "1816824a03520f434af9eee752c2cf01",
+    "url": "/react-carousel-comp/static/images/11.1816824a.jpg"
+  },
+  {
+    "revision": "99b8f78172851689ff1c",
+    "url": "/react-carousel-comp/static/css/main.7446bbe4.chunk.css"
+  },
+  {
+    "revision": "7c2d30b65aac36ab02d91c79f6d3ed77",
+    "url": "/react-carousel-comp/index.html"
+  }
+];
